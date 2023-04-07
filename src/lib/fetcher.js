72c39a5c9e60701111f358dcca126dddda807020
k-koech <koechkelvin97@gmail.com>
2023-04-07 08:22:29 +0300

    added vercel url

--- src/lib/fetcher.js
+++ src/lib/fetcher.js
@@ -3,12 +3,12 @@ import useSWR from 'swr';
 const response = (...args) => fetch(...args).then(res => res.json())
 // const baseURL = "http://127.0.0.1:3000/"
 
-// const baseURL ="https://blog-next-js-6p3x.vercel.app/"
+const baseURL ="https://blog-next-js-kappa-ruby.vercel.app/"
 // baseURL + 
 
 export default function Fetcher(endpoint)
 {
-    const {data, error} = useSWR(endpoint, response)
+    const {data, error} = useSWR(baseURL+endpoint, response)
     return {
         data,
         isLoading: !error && !data,
--- src/lib/helper.js
+++ src/lib/helper.js
@@ -1,8 +1,8 @@
 
 // const baseURL = "http://127.0.0.1:3000/api/posts"
-// const baseURL = "https://blog-next-js-6p3x.vercel.app/api/posts"
+const baseURL = "https://blog-next-js-kappa-ruby.vercel.app/api/posts"
 
-const baseURL = "/api/posts"
+// const baseURL = "/api/posts"
 
 export default async function GetPost(id){
     const res = await fetch(`${baseURL}`)
