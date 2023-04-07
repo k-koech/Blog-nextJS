23581ec354e1a5ad466719fd7181a726e6164271
k-koech <koechkelvin97@gmail.com>
2023-04-07 07:30:18 +0300

    added vercel url

--- src/lib/fetcher.js
+++ src/lib/fetcher.js
@@ -1,7 +1,8 @@
 import useSWR from 'swr';
 
 const response = (...args) => fetch(...args).then(res => res.json())
-const baseURL = "http://localhost:3000/"
+// const baseURL = "http://127.0.0.1:3000/"
+const baseURL ="https://blog-next-js-6p3x-3es6jsczi-k-koech.vercel.app"
 
 
 export default function Fetcher(endpoint)
